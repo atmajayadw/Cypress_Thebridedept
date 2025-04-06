72ae007568e24fc633c9700be1228ebe971a32c3
Jay <atmajaya.dw@gmail.com>
2025-04-07 00:35:11 +0700

    Create Test Script & Test Case Login with correct creds

---
+++ cypress/e2e/Login Test/login.cy.js
@@ -0,0 +1,34 @@
+describe("Login Page Test Cases", () => {
+    beforeEach(() => {
+      cy.viewport(1366, 768);
+    });
+  
+    it("Login with Correct Credentials", () => {
+        // User visit Website & click CTA Member
+        cy.visit("https://thebridedept.com/");    
+        cy.title().should("eq", "The Bride Dept");
+        cy.contains('a', 'Member').click();
+    
+        // Find Login Button, Email & Password field
+        cy.origin('https://member.thebridedept.com', () => {
+            // Find & Input Email
+            cy.get("input[name='email']").should("be.visible").type("atmajaya.dw@gmail.com");
+        
+            // Find and Input Password
+            cy.get("input[name='password']").should("be.visible").type("jay123");
+        
+            // Find and click button Login
+            cy.contains('button', 'Login').click();    
+        });
+
+        // Find and click User
+        cy.contains('a', 'Atmajaya').click({ force: true });
+
+        // Access Dashboard page & Assert
+        cy.origin('https://member.thebridedept.com', () => {
+            cy.contains('div', 'Latest articles you might like').should('be.visible');    
+        });
+
+    });
+    
+  });
