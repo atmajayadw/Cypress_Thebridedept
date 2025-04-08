ac13fea240bffa28771ba9f018ca0a014e34ce0b
Jay <atmajaya.dw@gmail.com>
2025-04-08 18:54:42 +0700

    Add Logout & Modular login

--- cypress/e2e/Login Test/login_logout.cy.js
+++ cypress/e2e/Login Test/login_logout.cy.js
@@ -30,5 +30,32 @@ describe("Login Page Test Cases", () => {
         });
 
     });
+
+    it("Logout", () => {
+        // User visit Website & click CTA Member
+        cy.visit("https://thebridedept.com/");    
+        cy.title().should("eq", "The Bride Dept");
+        cy.contains('a', 'Member').click();
+    
+        // Find Login Button, Email & Password field
+        cy.origin('https://member.thebridedept.com', () => {
+            // Find & Input Email
+            cy.get("input[name='email']").should("be.visible").type("atmajaya.dw@gmail.com");
+        
+            // Find and Input Password
+            cy.get("input[name='password']").should("be.visible").type("jay123");
+        
+            // Find and click button Login
+            cy.contains('button', 'Login').click();    
+        });
+
+        // Find and click User
+        cy.contains('a', 'Atmajaya').click({ force: true });
+
+        // Access Dashboard page & Assert
+        cy.origin('https://member.thebridedept.com', () => {
+            cy.contains('button', 'Logout').click();    
+        });     
+    });
     
   });
---
+++ cypress/e2e/Login Test/login_logout_modular.cy.js
@@ -0,0 +1,42 @@
+describe("Login Page Test Cases", () => {
+    // Simpen logic login di function
+    const login = () => {
+      cy.visit("https://thebridedept.com/");
+      cy.contains('a', 'Member').click();
+  
+      cy.origin('https://member.thebridedept.com', () => {
+        cy.get("input[name='email']").should("be.visible").type("atmajaya.dw@gmail.com");
+        cy.get("input[name='password']").should("be.visible").type("jay123");
+        cy.contains('button', 'Login').click();
+      });
+
+      cy.visit("https://thebridedept.com/");
+    };
+  
+    beforeEach(() => {
+      cy.viewport(1366, 768);
+  
+      // Jalankan login sekali dan reuse session-nya
+      cy.session("login-session", login);
+    });
+  
+    it("Login", () => {
+      cy.visit("https://thebridedept.com/");
+      cy.contains('a', 'Atmajaya').should('exist');
+      cy.contains('a', 'Atmajaya').click({ force: true });
+    });
+
+    it("Logout", () => {
+        cy.visit("https://thebridedept.com/");
+        cy.contains('a', 'Atmajaya').should('exist');
+        cy.contains('a', 'Atmajaya').click({ force: true });
+
+        cy.origin('https://member.thebridedept.com', () => {
+            cy.contains('button', 'Logout').click();    
+        }); 
+        
+      });
+  
+
+  });
+  
